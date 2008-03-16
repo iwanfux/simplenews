69c8cfcf8a6b9b264a7654f56fec0d57cb2bec3f
Erik Stielstra <info@erikstielstra.nl>
2008-03-16 17:02:58 +0000

    Variable name changed to $image

--- simplenews-status.tpl.php
+++ simplenews-status.tpl.php
@@ -6,11 +6,11 @@
  * Default theme implementation to display the simplenews newsletter status.
  *
  * Available variables:
- * - $img: status image
+ * - $image: status image
  * - $alt: 'alt' message
  * - $title: 'title' message
  *
  * @see template_preprocess_simplenews_status()
  */
 ?>
-  <img src="<?php print $image; ?>" width="15" height="15" alt="<?php print $alt; ?>" border="0" title="<?php print $title; ?>" />
+    <img src="<?php print $image; ?>" width="15" height="15" alt="<?php print $alt; ?>" border="0" title="<?php print $title; ?>" />
