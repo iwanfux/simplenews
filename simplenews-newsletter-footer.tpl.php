edadfaca42ced98bdfdadade6a8b9dc1269f7138
Erik Stielstra <info@erikstielstra.nl>
2009-09-30 14:10:20 +0000

    #183120 by Sutharsan, miro_dietiker: Added token support to Simplenews. Simplenews is now depending on Token.

--- simplenews-newsletter-footer.tpl.php
+++ simplenews-newsletter-footer.tpl.php
@@ -18,16 +18,16 @@
  * - $test_message: test message warning message
  *
  * Available tokens:
- * - !confirm_unsubscribe_url: unsubscribe url to be used as link
+ * - [confirm-unsubscribe-url]: unsubscribe url to be used as link
  * for more tokens: see simplenews_mail_tokens()
  *
  * @see template_preprocess_simplenews_newsletter_footer()
  */
 ?>
 <?php if ($format == 'html'): ?>
-  <p class="newsletter-footer"><a href="!confirm_unsubscribe_url"><?php print $unsubscribe_text ?></a></p>
+  <p class="newsletter-footer"><a href="[simplenews-unsubscribe-url]"><?php print $unsubscribe_text ?></a></p>
 <?php else: ?>
--- <?php print $unsubscribe_text ?>: !confirm_unsubscribe_url
+-- <?php print $unsubscribe_text ?>: [simplenews-unsubscribe-url]
 <?php endif ?>
 
 <?php if ($key == 'test'): ?>
