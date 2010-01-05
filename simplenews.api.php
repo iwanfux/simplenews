e6f251a904b6919a42a300f596637dd655f5d9fb
Erik Stielstra <info@erikstielstra.nl>
2010-01-05 07:44:14 +0000

    Added for API documentation

---
+++ simplenews.api.php
@@ -0,0 +1,21 @@
+<?php
+// $Id$
+
+/**
+ * @file
+ * Hooks provided by the Simplenews module.
+ */
+
+/**
+ * ...
+ */
+function hook_simplenews_issue_operations() {
+  
+}
+
+/**
+ * ...
+ */
+function hook_simplenews_subscription_operations() {
+  
+}
