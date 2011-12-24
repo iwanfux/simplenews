fbe420a228bb0e9cf595a4990c2318e2210ec7db
Sascha Grossenbacher <saschagros@gmail.com>
2011-12-24 01:11:22 +0100

    Issue #620498 by Berdir: Added basic rules integration.

--- includes/simplenews.api.php
+++ includes/simplenews.api.php
@@ -67,3 +67,29 @@ function hook_simplenews_subscriber_insert($subscriber) {
 function hook_simplenews_subscriber_delete($subscriber) {
 
 }
+
+/**
+ * Invoked if a user is subscribed to a newsletter.
+ *
+ * @param $subscriber
+ *   The subscriber object including all subscriptions of this user.
+ *
+ * @param $subscription
+ *   The subscription object for this specific subscribe action.
+ */
+function hook_simplenews_subscribe_user($subscriber, $subscription) {
+
+}
+
+/**
+ * Invoked if a user is unsubscribed from a newsletter.
+ *
+ * @param $subscriber
+ *   The subscriber object including all subscriptions of this user.
+ *
+ * @param $subscription
+ *   The subscription object for this specific unsubscribe action.
+ */
+function hook_simplenews_unsubscribe_user($subscriber, $subscription) {
+
+}
