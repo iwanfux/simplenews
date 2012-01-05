edbc1370ce1cc227862fc3a1fff29d4e772a0cc4
Sascha Grossenbacher <saschagros@gmail.com>
2012-01-05 08:21:10 +0100

    Issue #1392314 by Berdir: Cleaned up simplenews.api.php and created initial api doc groups.

--- simplenews.api.php
+++ simplenews.api.php
@@ -6,63 +6,141 @@
  */
 
 /**
+ * @mainpage Simplenews API documentation.
+ *
+ * Simplenews builds on the following basic concepts.
+ *
+ * @link subscriber Subscribers @endlink subscribe to @link newsletter
+ * newsletters (categories) @endlink. That connection is called
+ * a @link subscription subscription @endlink. Nodes of enabled content types
+ * are @link issue newsletter issues @endlink. These are then sent to the
+ * subscribers of the newsletter the issue is attached to.
+ *
+ * Sending is done by first adding a row for each subscriber to the @link spool
+ * mail spool @endlink.
+ * Then they are processed either immediatly or during cron runs. The actual
+ * sending happens through a @link source source instance @endlink, which is
+ * first instanciated based on the mail spool and then used to generated the
+ * actual mail content.
+ *
+ */
+
+/**
+ * @defgroup subscriber Subscriber
+ *
  * @todo
+ */
+
+/**
+ * @defgroup newsletter Newsletter (category)
+ *
+ * @todo
+ */
+
+/**
+ * @defgroup subscription Subscription
+ *
+ * @todo
+ */
+
+/**
+ * @defgroup issue Newsletter issue
+ *
+ * @todo
+ */
+
+/**
+ * @defgroup spool Mail spool
+ *
+ * @todo
+ */
+
+/**
+ * @defgroup source Source
+ *
+ * @todo
+ */
+
+/**
+ * Return operations to be applied to newsletter issues.
+ *
+ * @ingroup issue
  */
 function hook_simplenews_issue_operations() {
-
+  $operations = array(
+    'activate' => array(
+      'label' => t('Send'),
+      'callback' => 'simplenews_issue_send',
+    ),
+  );
+  return $operations;
 }
 
 /**
- * @todo
+ * Return operations to be applied to subscriptions.
+ *
+ * @ingroup issue
  */
 function hook_simplenews_subscription_operations() {
-
+  $operations = array(
+    'activate' => array(
+      'label' => t('Activate'),
+      'callback' => 'simplenews_subscription_activate',
+      'callback arguments' => array(SIMPLENEWS_SUBSCRIPTION_ACTIVE),
+    ),
+    'inactivate' => array(
+      'label' => t('Inactivate'),
+      'callback' => 'simplenews_subscription_activate',
+      'callback arguments' => array(SIMPLENEWS_SUBSCRIPTION_INACTIVE),
+    ),
+    'delete' => array(
+      'label' => t('Delete'),
+      'callback' => 'simplenews_subscription_delete_multiple',
+    ),
+  );
+  return $operations;
 }
 
 /**
- * @todo
- */
-function hook_simplenews_category_insert($category) {
-
-}
-
-/**
- * @todo
+ * Act after a newsletter category has been saved.
+ *
+ * @ingroup newsletter
  */
 function hook_simplenews_category_update($category) {
 
 }
 
 /**
- * @todo
+ * Act after a newsletter category has been deleted.
+ *
+ * @ingroup newsletter
  */
 function hook_simplenews_category_delete($category) {
 
 }
 
 /**
- * @todo
- */
-function hook_simplenews_mailing_list_insert($list) {
-
-}
-
-/**
- * @todo
+ * Act after a subscriber is updated.
+ *
+ * @ingroup subscriber
  */
 function hook_simplenews_subscriber_update($subscriber) {
 
 }
 
 /**
- * @todo
+ * Act after a new subscriber has been created.
+ *
+ * @ingroup subscriber
  */
 function hook_simplenews_subscriber_insert($subscriber) {
 
 }
 
 /**
- * @todo
+ * Act after a subscriber has been deleted.
+ *
+ * @ingroup subscriber
  */
 function hook_simplenews_subscriber_delete($subscriber) {
 
@@ -76,6 +154,8 @@ function hook_simplenews_subscriber_delete($subscriber) {
  *
  * @param $subscription
  *   The subscription object for this specific subscribe action.
+ *
+ * @ingroup subscriber
  */
 function hook_simplenews_subscribe_user($subscriber, $subscription) {
 
@@ -89,6 +169,8 @@ function hook_simplenews_subscribe_user($subscriber, $subscription) {
  *
  * @param $subscription
  *   The subscription object for this specific unsubscribe action.
+ *
+ * @ingroup subscriber
  */
 function hook_simplenews_unsubscribe_user($subscriber, $subscription) {
 
@@ -101,6 +183,8 @@ function hook_simplenews_unsubscribe_user($subscriber, $subscription) {
  *   An array keyed by the name of the class that provides the implementation,
  *   the array value consists of another array with the keys label and
  *   description.
+ *
+ * @ingroup source
  */
 function hook_simplenews_source_cache_info() {
   return array(
