2bc4fd720a5b8c5b4c140026bb927a9c3e66a755
Iwan Fux <ifux@redmouse.ch>
2014-07-28 17:53:08 +0200

    added issue field type

--- src/Plugin/Field/FieldType/IssueItem.php
+++ src/Plugin/Field/FieldType/IssueItem.php
@@ -2,38 +2,40 @@
 
 /**
  * @file
- * Contains \Drupal\simplenews\Plugin\Field\FieldType\SubscriptionItem.
+ * Contains \Drupal\simplenews\Plugin\Field\FieldType\IssueItem.
  */
 
 namespace Drupal\simplenews\Plugin\Field\FieldType;
 
+use Drupal\Core\TypedData\MapDataDefinition;
 use \Drupal\entity_reference\ConfigurableEntityReferenceItem;
 use Drupal\Core\Field\FieldStorageDefinitionInterface;
 use Drupal\Core\TypedData\DataDefinition;
 
 /**
- * Defines the 'subscription' entity field type (extended entity_reference).
+ * Defines the 'issue' entity field type (extended entity_reference).
  *
  * Supported settings (below the definition's 'settings' key) are:
  * - target_type: The entity type to reference. Required.
  * - target_bundle: (optional): If set, restricts the entity bundles which may
  *   may be referenced. May be set to an single bundle, or to an array of
  *   allowed bundles.
- * - status: A flag indicating whether the user is subscribed (1) or
- *   unsubscribed (0)
- * - timestamp: Time of when the user has (un)subscribed.
- * - source: The source via which the user has (un)subscribed.
+ * - handler: The issue handler.
+ * - handler_settings: The issue handler settings.
+ * - status: A flag indicating whether the issue is published (3), ready (2), pending (1) or
+ *   not (0)
+ * - sent_count: Counter of already sent newsletters.
  *
  * @FieldType(
- *   id = "simplenews_subscription",
- *   label = @Translation("Simplenews subscription"),
+ *   id = "simplenews_issue",
+ *   label = @Translation("Simplenews issue"),
  *   description = @Translation("An entity field containing an extended entityreference."),
  *   no_ui = TRUE,
- *   default_widget = "simplenews_subscription_select",
+ *   default_widget = "options_select",
  *   constraints = {"ValidReference" = {}}
  * )
  */
-class SubscriptionItem extends ConfigurableEntityReferenceItem {
+class IssueItem extends ConfigurableEntityReferenceItem {
 
   /**
    * {@inheritdoc}
@@ -42,15 +44,19 @@ class SubscriptionItem extends ConfigurableEntityReferenceItem {
     // Call the parent to define the target_id and entity properties.
     $properties = parent::propertyDefinitions($field_definition);
 
+    $properties['handler'] = DataDefinition::create('string')
+      ->setLabel(t('Handler'));
+
+    $properties['handler_settings'] = MapDataDefinition::create()
+      ->setLabel(t('Handler settings'));
+
     $properties['status'] = DataDefinition::create('integer')
       ->setLabel(t('Status'))
       ->setSetting('unsigned', TRUE);
-    
-    $properties['timestamp'] = DataDefinition::create('timestamp')
-      ->setLabel(t('Timestamp'));
 
-    $properties['source'] = DataDefinition::create('string')
-      ->setLabel(t('Source'));
+    $properties['sent_count'] = DataDefinition::create('integer')
+      ->setLabel(t('Sent count'))
+      ->setSetting('unsigned', TRUE);
 
     return $properties;
   }
@@ -61,22 +67,29 @@ class SubscriptionItem extends ConfigurableEntityReferenceItem {
   public static function schema(FieldStorageDefinitionInterface $field_definition) {
     $schema = parent::schema($field_definition);
 
+    $schema['columns']['handler'] = array(
+      'description' => 'The issue handler.',
+      'type' => 'varchar',
+      'length' => 255,
+      'not null' => FALSE,
+    );
+    $schema['columns']['handler_settings'] = array(
+      'description' => 'The issue handler settings.',
+      'type' => 'blob',
+      'size' => 'big',
+      'not null' => FALSE,
+      'serialize' => TRUE,
+    );
     $schema['columns']['status'] = array(
-      'description' => 'A flag indicating whether the user is subscribed (1) or unsubscribed (0).',
+      'description' => 'A flag indicating whether the issue is published (3), ready (2), pending (1) or not (0).',
       'type' => 'int',
       'size' => 'tiny',
       'not null' => FALSE,
     );
-    $schema['columns']['timestamp'] = array(
-      'description' => 'UNIX timestamp of when the user is (un)subscribed.',
+    $schema['columns']['sent_count'] = array(
+      'description' => 'Counter of already sent newsletters.',
       'type' => 'int',
       'unsigned' => TRUE,
-      'not null' => FALSE,
-    );
-    $schema['columns']['source'] = array(
-      'description' => 'The source via which the user is (un)subscription.',
-      'type' => 'varchar',
-      'length' => 24,
       'not null' => FALSE,
     );
     return $schema;
